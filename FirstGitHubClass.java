54796a4f63972248707b367424846454c745ee8c
shichong637740 <53772171+shichong637740@users.noreply.github.com>
2019-08-07 11:13:08 +0800

    Add files via upload

---
+++ FirstGitHubClass.java
@@ -0,0 +1,10 @@
+package cn.kgc.git;
+
+/**
+ * Created by shichong ON 2019/8/7 11:08
+ */
+public class FirstGitHubClass {
+    public static void main(String[] args){
+      System.out.println("hello,GitHub!");
+    }
+}
